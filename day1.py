4fe58eb3bd093db2c7c89a9a296d7b90ac9cbc91
Deepya <85464763+DeepyaNadimpalli@users.noreply.github.com>
2022-11-02 22:12:05 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -3,5 +3,5 @@
 print("Welcome to band name generator\n")
 city_name=input("Enter the city name:\n")
 pet_name=input("Enter the pet name:\n")
-band_name=city_name+" "+pet_name
-print("Your Band Name is "+band_name)
+band_name=city_name+" "+ pet_name
+print("Your Band Name is "+ band_name)
