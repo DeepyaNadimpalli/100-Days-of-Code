861760870268a0458a1a7e14da27bfcd915f5fa9
Deepya <85464763+DeepyaNadimpalli@users.noreply.github.com>
2022-11-02 22:11:02 +0530

    Add files via upload

--- day1.py
+++ day1.py
@@ -1,5 +1,6 @@
 #DAY1
 #BAND-NAME-GENERATOR
+print("Welcome to band name generator\n")
 city_name=input("Enter the city name:\n")
 pet_name=input("Enter the pet name:\n")
 band_name=city_name+" "+pet_name
