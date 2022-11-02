69668e16275fa5737947817bddc2230f52f3011f
Deepya <85464763+DeepyaNadimpalli@users.noreply.github.com>
2022-11-02 22:05:10 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,6 @@
+#DAY1
+#BAND-NAME-GENERATOR
+city_name=input("Enter the city name:\n")
+pet_name=input("Enter the pet name:\n")
+band_name=city_name+" "+pet_name
+print("Your Band Name is "+band_name)
